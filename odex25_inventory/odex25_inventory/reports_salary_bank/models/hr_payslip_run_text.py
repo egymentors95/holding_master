c617a3dddc75b4297a91e5f2477077a234a06b31
mohamed <ibs.developer1@gmail.com>
2025-11-12 15:46:08 +0200

    up

--- odex25_inventory/odex25_inventory/reports_salary_bank/models/hr_payslip_run_text.py
+++ odex25_inventory/odex25_inventory/reports_salary_bank/models/hr_payslip_run_text.py
@@ -80,7 +80,8 @@ class HrPayslipRunText(models.AbstractModel):
                 house_field = self._fmt_amount_numeric(getattr(slip, 'house_allowances', 0.0), 12)
                 collection_trans_other = slip.other_allowances + slip.trans_allowances
                 other_field = self._fmt_amount_numeric(collection_trans_other, 12)
-                deduction_field = self._fmt_amount_numeric(getattr(slip, 'total_deductions', 0.0), 12)
+                deduction_value = abs(getattr(slip, 'total_deductions', 0.0))
+                deduction_field = self._fmt_amount_numeric(deduction_value, 12)
                 currency_field = currency
                 five_zeros = '00000'
                 spaces50 = ' ' * 50
