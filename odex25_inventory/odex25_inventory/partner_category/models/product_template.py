b4108ddadde6e7e9c563d2d0a75ab3f34ffbc4a2
mohamed <ibs.developer1@gmail.com>
2025-12-18 02:34:08 +0200

    Asg

--- odex25_inventory/odex25_inventory/partner_category/models/product_template.py
+++ odex25_inventory/odex25_inventory/partner_category/models/product_template.py
@@ -1,12 +1,22 @@
-from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from odoo import api, fields, models, tools, _, SUPERUSER_ID
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    @tools.ormcache()
+    def _get_default_category_id(self):
+        # Deletion forbidden (at least through unlink)
+        return self.env.ref('product.product_category_all')
+
     product_category = fields.Integer(string='Product Category ID')
     utm_line_ids = fields.One2many(comodel_name='utm.lines', inverse_name='product_template_id', string='UTM Lines')
+    categ_id = fields.Many2one(
+        'product.category', 'Product Category',
+        change_default=True, default=_get_default_category_id, group_expand='_read_group_categ_id',
+        required=True, help="Select category for the current product",  company_dependent=True, check_company=True,)
+
 
 
 
