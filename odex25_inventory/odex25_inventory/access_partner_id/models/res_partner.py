56117a3dcd314796ec72fdc499649f32da7220ba
mohamed <ibs.developer1@gmail.com>
2026-01-03 13:49:38 +0200

    /

--- odex25_inventory/odex25_inventory/access_partner_id/models/res_partner.py
+++ odex25_inventory/odex25_inventory/access_partner_id/models/res_partner.py
@@ -5,3 +5,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     company_registration = fields.Char(string='CR')
+    sales_person_ids = fields.Many2many(comodel_name='res.users', string='Sales Persons')
