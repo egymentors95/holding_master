b2c12fde03849fe66496972fe3f15b9bc753148c
mohamed <ibs.developer1@gmail.com>
2026-01-10 16:02:58 +0200

    /

--- odex25_inventory/odex25_inventory/partner_category/models/account_move.py
+++ odex25_inventory/odex25_inventory/partner_category/models/account_move.py
@@ -7,3 +7,4 @@ class AccountMove(models.Model):
 
     purchase_order = fields.Many2one(comodel_name='purchase.order', string='P.O')
     description_note = fields.Char(string='Purchase Order')
+    description_a = fields.Char(string='Description')
