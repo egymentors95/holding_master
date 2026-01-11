5d39bfb78232950e51ded638e558819f63b4fb26
mohamed <ibs.developer1@gmail.com>
2026-01-11 15:55:30 +0200

    /

--- odex25_inventory/odex25_inventory/po_locations/models/sale_order.py
+++ odex25_inventory/odex25_inventory/po_locations/models/sale_order.py
@@ -17,7 +17,9 @@ class SaleOrder(models.Model):
             if stock_picking:
                 stock_picking.sale_id = order.id
                 for move in stock_picking.move_line_ids_without_package:
+                    # جلب أول line يطابق المنتج
                     line = order.order_line.filtered(lambda l: l.product_id == move.product_id)
                     if line:
-                        move.price_unit = line.price_unit
+                        move.price_unit = line[0].price_unit
         return res
+
